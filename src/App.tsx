cb5e858a4a8f325b8a174e79a06e559edbfaf386
郭晓东 <guoxiaodong@corp.netease.com>
2023-04-07 16:46:18 +0800

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { ProjectListScreen } from "screens/project-list";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <ProjectListScreen />
     </div>
   );
 }
-
-export default App;
